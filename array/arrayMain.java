08ae9edec827f65a964dc6fe7d00bb05bde35a93
rimonmostafiz <ferary.rimon@gmail.com>
2015-12-18 18:25:30 +0600

    string

--- array/arrayMain.java
+++ array/arrayMain.java
@@ -1,7 +1,7 @@
 /**
  * Created by rimon on 12/17/15.
  */
-public class main {
+public class arrayMain {
     public static void main(String[] args) {
         arrays myarrayobject = new arrays();
         myarrayobject.go();
---
+++ string/stringMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by rimon on 12/18/15.
+ */
+public class stringMain {
+    public static void main(String[] args) {
+        strings myStringObj = new strings();
+        myStringObj.go();
+    }
+}
