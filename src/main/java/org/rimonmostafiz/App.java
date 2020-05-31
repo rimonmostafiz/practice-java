10afac0730faa317d432aadae8d4d311a78e21df
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-06-01 01:45:57 +0600

    Reinventing the wheel. Again. :tada:

---
+++ src/main/java/org/rimonmostafiz/App.java
@@ -0,0 +1,11 @@
+package org.rimonmostafiz;
+
+
+/**
+ * @author Rimon Mostafiz
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
