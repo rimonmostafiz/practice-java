c2145ad6e4fda0d9003abab69a049e29b29732cd
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-06-01 01:56:39 +0600

    Add tests for methods of BitwiseOperations :white_check_mark:

---
+++ src/test/java/org/rimonmostafiz/bitwise/BitwiseOperationsShould.java
@@ -0,0 +1,51 @@
+package org.rimonmostafiz.bitwise;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * @author Rimon Mostafiz
+ */
+public class BitwiseOperationsShould {
+
+    private final BitwiseOperations bitwiseOperations = new BitwiseOperations();
+
+    @Test
+    public void convertToLowerCase() {
+        char ch = bitwiseOperations.toLowerCharacter('A');
+        Assertions.assertEquals('a', ch);
+
+        ch = bitwiseOperations.toLowerCharacter('z');
+        Assertions.assertEquals('z', ch);
+
+        ch = bitwiseOperations.toLowerCharacter('9');
+        Assertions.assertEquals('9', ch);
+    }
+
+    @Test
+    public void convertToUpperCase() {
+        char ch = bitwiseOperations.toUpperCharacter('a');
+        Assertions.assertEquals('A', ch);
+
+        ch = bitwiseOperations.toUpperCharacter('Z');
+        Assertions.assertEquals('Z', ch);
+
+        ch = bitwiseOperations.toUpperCharacter('1');
+        Assertions.assertEquals('1', ch);
+
+    }
+
+    @Test
+    public void encodeAndDecodeMessage() {
+        String message = "This is a message";
+        String encodedMessage = bitwiseOperations.encodeString(message);
+        Assertions.assertNotNull(encodedMessage);
+        Assertions.assertNotEquals(message, encodedMessage);
+
+        String decodedMessage = bitwiseOperations.decodeString(encodedMessage);
+        Assertions.assertNotNull(decodedMessage);
+        Assertions.assertNotEquals(encodedMessage, decodedMessage);
+        Assertions.assertEquals(message, decodedMessage);
+    }
+
+}
