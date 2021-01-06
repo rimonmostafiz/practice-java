5ec43a2d6f77936c2fd1416b065069faa9583a0a
Rimon Mostafiz <rimonmostafiz@gmail.com>
2021-01-06 15:17:40 +0600

    Solve hackerrank problem Ransom Note

---
+++ src/main/java/io/github/rimonmostafiz/hackerrank/interviewkit/hashtable/RansomNote.java
@@ -0,0 +1,73 @@
+package io.github.rimonmostafiz.hackerrank.interviewkit.hashtable;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RansomNote {
+
+    static void checkMagazine(String[] magazine, String[] note) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : magazine) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        for (String word : note) {
+            if (map.containsKey(word)) {
+                Integer val = map.get(word);
+                if (val > 0) {
+                    map.put(word, val - 1);
+                    continue;
+                } else {
+                    System.out.println("No");
+                    return;
+                }
+            } else {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
